3f9922a9f0bfa554121251ad40dd0e0319b6d285
Akbar Khamzaeff <66750440+Desper-18@users.noreply.github.com>
2022-04-22 14:33:01 +0500

    Add files via upload

---
+++ public/Barman/delete.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Database\Connection;
+use App\Database\QueryBuilder;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$config = require_once '../config.php';
+$pdo = Connection::make($config);
+$query = new QueryBuilder($pdo);
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $query->delete("cocktails", $id);
+    header('Location: /32_crud_all_four/public/Barman/index.php');
+}
